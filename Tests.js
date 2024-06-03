58836e1c9d53c762b76a6dcfcba020bcc31807cc
Yotam Efrati <yotamefrati05@gmail.com>
2024-06-03 14:17:51 +0300

    2

--- Tests.js
+++ Tests.js
@@ -1,143 +1,209 @@
 import React, { Component } from 'react';
-import { Animated, PanResponder, View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet, Dimensions } from 'react-native';
 import Svg, { Circle, Text as SvgText } from 'react-native-svg';
 import axios from 'axios';
-import {sendApiPostRawBody, sendApiPostRequest} from "./ApiRequests";
+import Draggable from 'react-native-draggable';
 
-class Tests extends Component {
+const { width: screenWidth } = Dimensions.get('window');
+
+class Tests2 extends Component {
     state = {
         oldPhases: [],
         currentPhase: [
-            { x: -100, y: 100 },
-            { x: 100, y: 100 },
-            { x: 100, y: 300 },
-            { x: -100, y: 300 },
-            { x: 0, y: 200 }
-        ]
-    };
-
-    // Things related to draggable circles
-    position = this.state.currentPhase.map((item) => new Animated.ValueXY({ x: item.x, y: item.y }));
-
-    createPanResponder = (position, index) => {
-        return PanResponder.create({
-            onStartShouldSetPanResponder: () => true,
-            onPanResponderMove: this.onPanResponderMove(position),
-            onPanResponderRelease: this.onPanResponderRelease(position, index),
-            onPanResponderGrant: this.onPanResponderGrant(position)
-        });
-    };
-
-    onPanResponderMove = (position) => Animated.event(
-        [
-            null,
-            { dx: position.x, dy: position.y }
+            { x: -100, y: 100, action: 0, draggable:false},
+            { x: 100, y: 100, action: 0, draggable:false},
+            { x: 100, y: 300, action: 0, draggable:false},
+            { x: -100, y: 300, action: 0, draggable:false},
+            { x: 0, y: 200, action: 0, draggable:false}
         ],
-        { useNativeDriver: false }
-    );
-
-    onPanResponderRelease = (position, index) => () => {
-        position.flattenOffset();
-        this.setState(prevState => {
-            const newPhase = [...prevState.currentPhase];
-            newPhase[index] = {
-                x: position.x._value,
-                y: position.y._value
-            };
-            return { currentPhase: newPhase };
-        });
+        selectedCircle: null,
+        menuVisible: false,
     };
 
-    onPanResponderGrant = (position) => () => {
-        position.setOffset({
-            x: position.x._value,
-            y: position.y._value
-        });
-        position.setValue({ x: 0, y: 0 });
-    };
-
-    panResponders = this.position.map((position, index) => this.createPanResponder(position, index));
-    //end of draggable stuff
-
-
-
-    // Method to send the current phase data to the server
     sendPhaseToServer = () => {
         const playerPhases = this.state.currentPhase.map((phase, index) => ({
             playerNumber: index + 1,
-            phase: null,
             hasBall: false,
             x: phase.x,
             y: phase.y,
-            action: 0
+            action: phase.action
         }));
 
-        sendApiPostRawBody("http://10.0.0.8:8989" + '/add-phase', {
+        axios.post("http://10.0.0.8:8989/add-phase", {
             secret: "630d73f0-16b7-4ecf-b44f-4192dfb96d1e",
             playName: "three",
             orderNum: 1,
             playerPhases
-        }, (response) => {
-            console.log('Response:', response.data);
-            if (response.data.success) {
-                console.log('OK');
-            }
-            this.setState({errorCode: response.data.errorCode})
         })
+            .then(response => {
+                console.log('Response:', response.data);
+                if (response.data.success) {
+                    console.log('OK');
+                }
+                this.setState({ errorCode: response.data.errorCode });
+            })
+            .catch(error => {
+                console.error('Error sending phase data:', error);
+            });
     };
+
+
+
+
+    handleCircleClick = (index) => {
+        if (!this.state.currentPhase[index].draggable) {
+            this.setState({
+                selectedCircle: index,
+                menuVisible: !this.state.menuVisible
+            });
+        }
+    };
+
+    handleMenuClick = (action) => {
+        if (!this.state.currentPhase[this.state.selectedCircle].draggable) {
+            this.setState(prevState => {
+                const newPhase = [...prevState.currentPhase];
+                newPhase[prevState.selectedCircle].action = action;
+                newPhase[prevState.selectedCircle].draggable = true;
+                return {currentPhase: newPhase, menuVisible: false};
+            });
+        }
+    };
+
+
+    handleDragRelease = (index, event, gestureState, bounds) => {
+        console.log(this.state.currentPhase[index].draggable)
+        if(this.state.currentPhase[index].draggable) {
+            const {moveX, moveY} = gestureState;
+            this.setState(prevState => {
+                const newPhase = prevState.currentPhase.map(item => {
+                    if (item.index === index) {
+                        return {...item, x: moveX, y: moveY};
+                    }
+                    return item;
+                });
+
+                return {currentPhase: newPhase};
+            });
+        }
+
+
+    };
+
 
 
     render() {
         return (
             <View style={{ flex: 1 }}>
+
+                {/*circles*/}
                 {this.state.currentPhase.map((item, index) => (
-                    <Animated.View
+                    <Draggable
                         key={index}
-                        style={{
-                            position: 'absolute',
-                            transform: this.position[index].getTranslateTransform()
-                        }}
-                        {...this.panResponders[index].panHandlers}
+                        x={item.x}
+                        y={item.y}
+                        disabled={!this.state.currentPhase[index].draggable}
+                        onDragRelease={(e, gestureState, bounds) => this.handleDragRelease(index, e, gestureState, bounds)}
                     >
-                        <Svg height="100" width="100">
+                        <Svg height="100" width="100" onPress={() => this.handleCircleClick(index)}>
                             <Circle
                                 cx="50"
                                 cy="50"
                                 r="25"
                                 stroke="black"
                                 strokeWidth="2.5"
-                                fill="rgba(0, 0, 0, 0.05)" // 10% transparency
+                                fill="rgba(0, 0, 0, 0.05)"
                             />
                             <SvgText
-                                x="49.5"
-                                y="58"
+                                x="50"
+                                y="55"
                                 fontSize="25"
                                 fill="black"
                                 textAnchor="middle"
-                                fontWeight="bold" // Bold text
+                                fontWeight="bold"
                             >
                                 {index + 1}
                             </SvgText>
                         </Svg>
-                    </Animated.View>
+                    </Draggable>
                 ))}
+
+                {/*button*/}
                 <TouchableOpacity
-                    style={{
-                        position: 'absolute',
-                        bottom: 50,
-                        left: '50%',
-                        marginLeft: -50,
-                        backgroundColor: 'blue',
-                        padding: 10,
-                        borderRadius: 5
-                    }}
+                    style={styles.sendPhaseButton}
                     onPress={this.sendPhaseToServer}
                 >
                     <Text style={{ color: 'white' }}>Send Phase</Text>
                 </TouchableOpacity>
+
+                {/*menu*/}
+                {this.state.menuVisible && (
+                    <View style={styles.menuContainer}>
+                        <View style={styles.menu}>
+                            <TouchableOpacity style={styles.menuItem} onPress={() => this.handleMenuClick(1)}>
+                                <Text style={styles.menuText}>Cut</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.menuItem} onPress={() => this.handleMenuClick(2)}>
+                                <Text style={styles.menuText}>Dribble</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.menuItem} onPress={() => this.handleMenuClick(3)}>
+                                <Text style={styles.menuText}>Screen</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                )}
+
+                {/*debug stuff*/}
+                <Text style={styles.debugText}>Actions: {this.state.currentPhase.map((item, index) => (
+                    <Text key={index}>{item.action} </Text>
+                ))}</Text>
             </View>
         );
     }
 }
 
-export default Tests;
+const styles = StyleSheet.create({
+    sendPhaseButton: {
+        position: 'absolute',
+        bottom: 50,
+        left: '50%',
+        marginLeft: -50,
+        backgroundColor: 'blue',
+        padding: 10,
+        borderRadius: 5
+    },
+    menuContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: screenWidth / 2 - screenWidth / 3,
+        width: screenWidth / 3,
+        backgroundColor: 'rgba(241, 241, 241, 0.5)',
+        padding: 10,
+    },
+    menu: {
+        backgroundColor: 'white',
+        borderRadius: 5,
+    },
+    menuItem: {
+        padding: 10,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        flex: 1,
+        alignItems: 'center',
+        marginVertical: 5
+    },
+    menuText: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    debugText: {
+        position: 'absolute',
+        top: 50,
+        left: '50%',
+        marginLeft: -75,
+        fontSize: 16,
+        color: 'red'
+    }
+});
+
+export default Tests2;
