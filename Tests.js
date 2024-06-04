48130528c8932abc6d79248c501ffdf4a4b91e3f
Yotam Efrati <yotamefrati05@gmail.com>
2024-06-04 15:51:09 +0300

    2

--- Tests.js
+++ Tests.js
@@ -4,21 +4,26 @@ import Svg, { Circle, Text as SvgText } from 'react-native-svg';
 import axios from 'axios';
 import Draggable from 'react-native-draggable';
 
-const { width: screenWidth } = Dimensions.get('window');
+const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
-class Tests2 extends Component {
+class Tests extends Component {
     state = {
+        setInitialPosition: true,
         oldPhases: [],
         currentPhase: [
-            { x: -100, y: 100, action: 0, draggable:false},
-            { x: 100, y: 100, action: 0, draggable:false},
-            { x: 100, y: 300, action: 0, draggable:false},
-            { x: -100, y: 300, action: 0, draggable:false},
-            { x: 0, y: 200, action: 0, draggable:false}
+            { x: 0, y: 0, action: 0, draggable: true },
+            { x: 0, y: 0, action: 0, draggable: true },
+            { x: 0, y: 0, action: 0, draggable: true },
+            { x: 0, y: 0, action: 0, draggable: true },
+            { x: 0, y: 0, action: 0, draggable: true }
         ],
         selectedCircle: null,
         menuVisible: false,
     };
+
+    componentDidMount() {
+        console.log("Initial positions:", this.state.currentPhase);
+    }
 
     sendPhaseToServer = () => {
         const playerPhases = this.state.currentPhase.map((phase, index) => ({
@@ -45,13 +50,40 @@ class Tests2 extends Component {
             .catch(error => {
                 console.error('Error sending phase data:', error);
             });
+
+        this.prepareNewPhase();
+
     };
 
+    prepareNewPhase = () => {
+        this.setState(prevState => {
+            const newPhases = prevState.currentPhase.map(phase => ({
+                ...phase,
+                draggable: false,
+                action: 0
+            }));
+            return {
+                currentPhase: newPhases,
+            };
+        });
+    }
 
-
+    setInitialPosition = () => {
+        this.setState(prevState => {
+            const newPhases = prevState.currentPhase.map(phase => ({
+                ...phase,
+                draggable: false
+            }));
+            return {
+                currentPhase: newPhases,
+                setInitialPosition: false
+            };
+        });
+        console.log(this.state.currentPhase);
+    };
 
     handleCircleClick = (index) => {
-        if (!this.state.currentPhase[index].draggable) {
+        if (!this.state.currentPhase[index].draggable && !this.state.setInitialPosition) {
             this.setState({
                 selectedCircle: index,
                 menuVisible: !this.state.menuVisible
@@ -60,50 +92,39 @@ class Tests2 extends Component {
     };
 
     handleMenuClick = (action) => {
-        if (!this.state.currentPhase[this.state.selectedCircle].draggable) {
+        if (!this.state.currentPhase[this.state.selectedCircle].draggable && !this.state.setInitialPosition) {
             this.setState(prevState => {
                 const newPhase = [...prevState.currentPhase];
                 newPhase[prevState.selectedCircle].action = action;
                 newPhase[prevState.selectedCircle].draggable = true;
-                return {currentPhase: newPhase, menuVisible: false};
+                return { currentPhase: newPhase, menuVisible: false };
             });
         }
     };
 
+    handleDragRelease = (index, event, gestureState) => {
+        if (this.state.currentPhase[index].draggable) {
+            const newX = this.state.currentPhase[index].x + gestureState.dx;
+            const newY = this.state.currentPhase[index].y + gestureState.dy;
 
-    handleDragRelease = (index, event, gestureState, bounds) => {
-        console.log(this.state.currentPhase[index].draggable)
-        if(this.state.currentPhase[index].draggable) {
-            const {moveX, moveY} = gestureState;
+            console.log(newX+","+newY);
             this.setState(prevState => {
-                const newPhase = prevState.currentPhase.map(item => {
-                    if (item.index === index) {
-                        return {...item, x: moveX, y: moveY};
-                    }
-                    return item;
-                });
-
-                return {currentPhase: newPhase};
+                const newPhase = [...prevState.currentPhase];
+                newPhase[index].x = newX;
+                newPhase[index].y = newY;
+                return { currentPhase: newPhase };
             });
         }
-
-
     };
-
-
 
     render() {
         return (
-            <View style={{ flex: 1 }}>
-
-                {/*circles*/}
+            <View style={styles.container} height={screenHeight} width={screenWidth}>
                 {this.state.currentPhase.map((item, index) => (
                     <Draggable
                         key={index}
-                        x={item.x}
-                        y={item.y}
-                        disabled={!this.state.currentPhase[index].draggable}
-                        onDragRelease={(e, gestureState, bounds) => this.handleDragRelease(index, e, gestureState, bounds)}
+                        disabled={!item.draggable}
+                        onDragRelease={(e, gestureState) => this.handleDragRelease(index, e, gestureState)}
                     >
                         <Svg height="100" width="100" onPress={() => this.handleCircleClick(index)}>
                             <Circle
@@ -128,15 +149,22 @@ class Tests2 extends Component {
                     </Draggable>
                 ))}
 
-                {/*button*/}
                 <TouchableOpacity
                     style={styles.sendPhaseButton}
                     onPress={this.sendPhaseToServer}
+                    disabled={this.state.setInitialPosition}
                 >
                     <Text style={{ color: 'white' }}>Send Phase</Text>
                 </TouchableOpacity>
 
-                {/*menu*/}
+                <TouchableOpacity
+                    style={styles.setInitialPositionButton}
+                    onPress={this.setInitialPosition}
+                    disabled={!this.state.setInitialPosition}
+                >
+                    <Text style={{ color: 'white' }}>Set initial position</Text>
+                </TouchableOpacity>
+
                 {this.state.menuVisible && (
                     <View style={styles.menuContainer}>
                         <View style={styles.menu}>
@@ -153,9 +181,8 @@ class Tests2 extends Component {
                     </View>
                 )}
 
-                {/*debug stuff*/}
-                <Text style={styles.debugText}>Actions: {this.state.currentPhase.map((item, index) => (
-                    <Text key={index}>{item.action} </Text>
+                <Text style={styles.debugText}>Phase: {this.state.currentPhase.map((item, index) => (
+                    <Text key={index}>{item.x},{item.y} </Text>
                 ))}</Text>
             </View>
         );
@@ -163,6 +190,11 @@ class Tests2 extends Component {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#4B9CD3', // Changed background color to make circles more visible
+    },
+
     sendPhaseButton: {
         position: 'absolute',
         bottom: 50,
@@ -172,10 +204,19 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 5
     },
+    setInitialPositionButton: {
+        position: 'absolute',
+        bottom: 50,
+        left: '0%',
+        marginLeft: 0,
+        backgroundColor: 'blue',
+        padding: 10,
+        borderRadius: 5
+    },
     menuContainer: {
         position: 'absolute',
         bottom: 0,
-        left: screenWidth / 2 - screenWidth / 3,
+        left: '67%',
         width: screenWidth / 3,
         backgroundColor: 'rgba(241, 241, 241, 0.5)',
         padding: 10,
@@ -206,4 +247,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default Tests2;
+export default Tests;
